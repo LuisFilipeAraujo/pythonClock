a35e0054677c03549db688adad85e1463965d3cf
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-06-02 18:02:37 -0300

    added root configs and tela configs

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from tkinter import *
 import os
 from time import strftime
 
+root = tk.Tk()
+root.title('Seu Relógio')
+root.geometry("600x300")
+root.maxsize(600,320)
+root.minsize(600,320)
+root.configure(background='#1d1d1d')
+
+tela = tk.Canvas(root, width=600, height=60, bg='#1d1d1d', bd=0, highlightthickness=0, relief='ridge')
+tela.pack()
